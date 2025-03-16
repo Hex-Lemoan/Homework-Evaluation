ace0a857fbbe3d683476b207b12f57107b00f371
Hex-Lemoan <emon2005.bd@gmail.com>
2025-03-16 21:51:50 +0900

    Add files via upload

---
+++ about_me.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int main() {
+    printf("[Magrathea] ❤❤ Project Member Introduction ❤❤\n");
+    printf("==========================================================================================\n");
+    // Arthur Dent
+    printf("\t\tName           | Arthur Dent\t\t                      Gender\t\t           | Male\n");
+    printf("\t\tMajor          | Mechanical Engineering                       Experience\t\t   | 00000000 00000000 00000000 00001111 years\n");
+    printf("\t\tRole           | Architect                                    Skills\t\t           | C/C++, Java, Spring, Python, Machine Learning/Deep Learning\n");
+    printf("------------------------------------------------------------------------------------------\n");
+    printf("\t\tIntroduction   | I built my career working on various projects in SNS, fintech, HR, and media in Silicon Valley.\n");
+    printf("                 Through successes and failures, I have gained insights I want to share with you.\n");
+    printf("==========================================================================================\n");
+    // Kim Youngjin
+    printf("\t\tName           | Kim Youngjin\t\t                      Gender\t\t           | Male\n");
+    printf("\t\tMajor          | Computer Engineering                         Experience\t\t   | 00000000 00000000 00000000 00000010 years\n");
+    printf("\t\tRole           | Developer                                    Skills\t\t           | C#, Python, JavaScript, React, Kotlin\n");
+    printf("------------------------------------------------------------------------------------------\n");
+    printf("\t\tIntroduction   | Recently, I found immense passion for software development through a personal asset\n");
+    printf("                 management project using the MAUI framework. Solving real-world problems with my code\n");
+    printf("                 is incredible! Successfully developing Magrathea has now become one of my bucket list goals.\n");
+    printf("==========================================================================================\n");
+    // Im Woncheol
+    printf("\t\tName           | Im Woncheol\t\t                      Gender\t\t           | Male\n");
+    printf("\t\tMajor          | Electronic Engineering                       Experience\t\t   | 00000000 00000000 00000000 00000001 years\n");
+    printf("\t\tRole           | DBA                                          Skills\t\t           | Java, PHP, MongoDB, MS SQL, MySQL\n");
+    printf("------------------------------------------------------------------------------------------\n");
+    printf("\t\tIntroduction   | I believe that applications are ultimately about how they handle data. While studying\n");
+    printf("                 Big Data courses in university, I developed a deep interest in data platforms. I think\n");
+    printf("                 the success of this project depends on data. Just thinking about the data we’ll manage\n");
+    printf("                 in Magrathea already excites me!\n");
+    printf("==========================================================================================\n");
+    // Yoo Goeun
+    printf("\t\tName           | Yoo Goeun\t\t                      Gender\t\t           | Female\n");
+    printf("\t\tMajor          | Management Information Systems               Experience\t\t   | 00000000 00000000 00000000 00000001 years\n");
+    printf("\t\tRole           | Cloud Engineer                               Skills\t\t           | Java, PowerShell, Azure, AWS, GCP\n");
+    printf("------------------------------------------------------------------------------------------\n");
+    printf("\t\tIntroduction   | I worked on a project with Kim Youngjin, where I was responsible for implementing\n");
+    printf("                 cloud architecture. During my school years, I became fascinated with cloud computing,\n");
+    printf("                 particularly Microsoft Azure. As I delved deeper, I found that understanding other\n");
+    printf("                 public cloud platforms became much easier. I am determined to complete Magrathea's\n");
+    printf("                 cloud architecture with my own hands!\n");
+    printf("==========================================================================================\n");
+    // Seo Hyekyung
+    printf("\t\tName           | Seo Hyekyung\t\t                      Gender\t\t           | Female\n");
+    printf("\t\tMajor          | Political Science & International Relations  Experience\t\t   | 00000000 00000000 00000000 00000000 years\n");
+    printf("\t\tRole           | Developer                                    Skills\t\t           | Python, Swift, Kotlin, Node.js, Figma\n");
+    printf("------------------------------------------------------------------------------------------\n");
+    printf("\t\tIntroduction   | While I have worked on many projects as academic assignments, this is my first real-world\n");
+    printf("                 project where a company's success is at stake. I am both excited and nervous about whether\n");
+    printf("                 I can perform well. When I first heard about the Magrathea project, something inside me\n");
+    printf("                 told me that this was a project worth dedicating myself to. Having already heard of Arthur’s\n");
+    printf("                 reputation through LinkedIn, I feel honored to be part of this journey.\n");
+    printf("==========================================================================================\n");
+    
+    return 0;
+}
